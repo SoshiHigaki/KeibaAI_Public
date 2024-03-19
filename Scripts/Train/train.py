31907da9069a83d54b6db0bc53929d107b4b455b
SoshiHigaki <145969282+SoshiHigaki@users.noreply.github.com>
2024-03-19 15:10:36 +0900

    Add files via upload

--- Scripts/Train/train.py
+++ Scripts/Train/train.py
@@ -4,9 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import pandas as pd
+import os
 
 from Scripts.Train import model
 from Scripts.Train import dataset
+
 
 class Config(object):
     def __init__(self):
@@ -29,18 +31,19 @@ class Train(object):
         self.optimizer = torch.optim.Adam(self.ve_net.parameters(), lr=0.01)
 
         self.batch_size = 2**10
+        self.log_folder = 'Log/'
 
     def train(self, paths, epochs):
         self.ve_net.train()
 
-        history = pd.DataFrame([])
         for path in tqdm(paths):
             self.dataset.prepare(path)
             data_loader = DataLoader(self.dataset,
                                      batch_size=self.batch_size,
                                      shuffle=True)
-            
-            tmp_history = []
+            path_list = path.split('/')
+            log_path = f'{self.log_folder}/{path_list[-2]}-{path_list[-1]}'
+            history = list(pd.read_pickle(log_path)['loss']) if os.path.exists(log_path) else []
             for _ in tqdm(range(epochs), leave=False):
                 epoch_loss = 0
                 for data in data_loader:
@@ -55,15 +58,10 @@ class Train(object):
                     self.optimizer.step()
                     self.optimizer.zero_grad()
 
-                tmp_history.append(epoch_loss / len(data_loader))
+                history.append(epoch_loss / len(data_loader))
                 
-
-            history[path] = tmp_history
-
+            history_df = pd.DataFrame({'epochs':list(range(1, len(history)+1, 1)), 'loss':history})
+            history_df.to_pickle(log_path)
+            
         return history
                 
-
-
-
-
-        
